07bc16879bdfc893405e44ee78d1a35593aa932e
mflem8 <matthewfleming8@gmail.com>
2021-02-23 21:54:12 -0500

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
-  
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :reviews
   resources :cigars
   resources :brands
