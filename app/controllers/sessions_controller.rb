d9a645ffdc107a53e645259a6b634ee1c8ba63bf
mflem8 <matthewfleming8@gmail.com>
2021-02-23 21:54:02 -0500

    built sessions controller and view(s)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,26 @@
 class SessionsController < ApplicationController
 
     def welcome
-        
+
+    end
+
+    def new
+
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Invalid login credentials. Please try again."
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
     end
 end
