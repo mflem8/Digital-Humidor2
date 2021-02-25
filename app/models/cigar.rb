22d155d8d00843588901c3abe7a08be840142773
mflem8 <matthewfleming8@gmail.com>
2021-02-24 22:37:20 -0500

    added nested route/form

--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -1,6 +1,9 @@
 class Cigar < ApplicationRecord
   belongs_to :brand
   belongs_to :user
+  
   has_many :reviews
   has_many :users, through: :reviews
+
+  accepts_nested_attributes_for :brand
 end
