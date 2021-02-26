c6196bd86dccc75a68088cc359e2fe5180d70c11
mflem8 <matthewfleming8@gmail.com>
2021-02-25 21:18:22 -0500

    additional model validations

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,5 @@
 class Brand < ApplicationRecord
     has_many :cigars
+
+    validates :name, presence: true
 end
--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -1,9 +1,11 @@
 class Cigar < ApplicationRecord
   belongs_to :brand
   belongs_to :user
-  
+
   has_many :reviews
   has_many :users, through: :reviews
 
   accepts_nested_attributes_for :brand
+
+  validates :name, :style, :brand, presence: true
 end
