b418dcb110743896094bdae5ff1bc69b32a10b06
mflem8 <matthewfleming8@gmail.com>
2021-02-28 21:38:30 -0500

    add class method to prevent duplicate user entries

--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -7,5 +7,13 @@ class Cigar < ApplicationRecord
   accepts_nested_attributes_for :brand
 
   validates :name, :style, presence: true
+  validate :not_a_duplicate
+
+  def not_a_duplicate
+    cigar = Cigar.find_by(style: style, brand_id: brand_id)
+    if !!cigar && cigar != self
+      errors.add(:name, 'has already been added for that brand!')
+    end
+  end
   
 end
